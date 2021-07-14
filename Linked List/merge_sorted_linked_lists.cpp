a91e9408900639ef59bc39af5a156b62068d915e
usgn94 <60835892+usgn94@users.noreply.github.com>
2021-07-14 17:20:52 +0530

    Update merge_sorted_linked_lists.cpp

--- Linked List/merge_sorted_linked_lists.cpp
+++ Linked List/merge_sorted_linked_lists.cpp
@@ -21,3 +21,45 @@ Node* sortedMerge(Node* head1, Node* head2)
     
     
 }
+//Metho2
+
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    Node* head = NULL;
+    Node* temp = NULL;
+    
+    while(head1!= NULL && head2!= NULL){
+        if(head1->data < head2->data){
+            Node*temp1 = head1->next;
+            head1->next = NULL;
+            if(head==NULL){
+                head = head1;
+                temp = head;
+            }
+            else{
+                temp->next = head1;
+                temp = temp->next;
+            }
+            head1 = temp1;
+        }
+        else{
+            Node*temp1 = head2->next;
+            head2->next = NULL;
+            if(head==NULL){
+                head = head2;
+                temp = head;
+            }
+            else{
+                temp->next = head2;
+                temp = temp->next;
+            }
+            head2 = temp1;
+        }
+        //cout<<temp->data<<" ";
+    }
+    temp->next = (head1)?head1:head2;
+    
+    return head;
+    
+    
+}  
